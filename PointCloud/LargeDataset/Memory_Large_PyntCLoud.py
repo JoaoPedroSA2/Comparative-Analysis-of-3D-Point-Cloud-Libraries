e66c671153006f79827ff286f574521e19bf8dfa
Joao Pedro <jpsa101106@gmail.com>
2026-07-19 16:46:15 -0300

    Pyncloud benchmark

---
+++ PointCloud/LargeDataset/Memory_Large_PyntCLoud.py
@@ -0,0 +1,75 @@
+from pyntcloud import PyntCloud
+import psutil
+import time
+import os
+import gc
+import statistics
+
+def medir_memoria():
+    return psutil.Process(os.getpid()).memory_info().rss / (1024*1024)
+
+#leitura do arquivo
+gc.collect()
+
+tempos = []
+memorias = []
+
+for i in range(3):
+    point_cloud = PyntCloud.from_file(r"C:\Users\joao.santana\Downloads\lucy.ply\lucy.ply")
+    
+    del point_cloud
+    gc.collect()
+
+for j in range(15):
+    memoria_antes = medir_memoria()
+    
+    start = time.time()
+    
+    point_cloud = PyntCloud.from_file(r"C:\Users\joao.santana\Downloads\lucy.ply\lucy.ply")
+    
+    memoria_final = medir_memoria()
+    
+    end = time.time()
+    
+    tempos.append(end-start)
+    memorias.append(memoria_final - memoria_antes)
+    
+    del point_cloud
+    gc.collect()
+
+print("tempo gasto: ", statistics.mean(tempos))
+print("memoria gasta: ", statistics.mean(memorias))
+
+#downsampling
+gc.collect()
+
+tempos = []
+memorias = []
+
+point_cloud = PyntCloud.from_file(r"C:\Users\joao.santana\Downloads\lucy.ply\lucy.ply")
+print("pontos originais: ", point_cloud.points)
+
+for j in range(15):
+    memoria_antes = medir_memoria()
+    
+    start = time.time()
+    
+    voxelgrid_id = point_cloud.add_structure("voxelgrid", size_x = 5, size_y = 5, size_z = 5)
+    
+    voxelgrid = point_cloud.structures[voxelgrid_id]
+    
+    sample = point_cloud.get_sample("voxelgrid_centers", voxelgrid_id = voxelgrid_id)
+    
+    memoria_final = medir_memoria()
+    
+    end = time.time()
+    
+    tempos.append(end-start)
+    memorias.append(memoria_final - memoria_antes)
+    
+    del point_cloud.structures[voxelgrid_id]
+    del voxelgrid_id, voxelgrid, sample,
+    gc.collect()
+
+print("tempo medio gasto: ", statistics.mean(tempos))
+print("memoria media gasta: ", statistics.mean(memorias))
---
+++ PointCloud/MediumDataset/CPU_Medium_Pyntcloud.py
@@ -0,0 +1,93 @@
+from pyntcloud import PyntCloud
+import psutil
+import time
+import os
+import gc
+import statistics  
+
+#leitura do arquivo
+process = psutil.Process(os.getpid())
+num_cpus = psutil.cpu_count()
+
+tempo = []
+cpus_porcentagem = []
+cpus_tempo = []
+
+for i in range(3):
+    point_cloud = PyntCloud.from_file(r"C:\Users\joao.santana\Downloads\xyzrgb_statuette.ply\xyzrgb_statuette.ply")
+
+for j in range(15):
+    start = time.perf_counter()
+    
+    cpu_antes = process.cpu_times()
+    
+    point_cloud = PyntCloud.from_file(r"C:\Users\joao.santana\Downloads\xyzrgb_statuette.ply\xyzrgb_statuette.ply")
+    
+    cpu_final = process.cpu_times()
+    
+    end = time.perf_counter()
+    
+    tempo_total = (end-start)
+    
+    cpu_total = ((cpu_final.user - cpu_antes.user) + (cpu_final.system - cpu_antes.system))
+    cpu_media = (cpu_total/(end-start)) * 100
+    
+    tempo.append(tempo_total)
+    cpus_porcentagem.append(cpu_media)
+    cpus_tempo.append(cpu_total)
+    
+
+print(f"Tempo médio: {statistics.mean(tempo):.5f}s")
+print(f"Tempo mediano: {statistics.median(tempo):.5f}s")
+
+print(f"CPU média (%): {statistics.mean(cpus_porcentagem):.2f}%")
+print(f"CPU mediana (%): {statistics.median(cpus_porcentagem):.2f}%")
+
+print(f"CPU tempo médio: {statistics.mean(cpus_tempo):.5f}s")
+
+#downsampling
+process = psutil.Process(os.getpid())
+
+tempo = []
+cpus_porcentagem = []
+cpus_tempo = []
+
+point_cloud = PyntCloud.from_file(r"C:\Users\joao.santana\Downloads\xyzrgb_statuette.ply\xyzrgb_statuette.ply")
+
+for i in range(3):
+    voxelgrid_id = point_cloud.add_structure("voxelgrid", size_x = 5, size_y = 5, size_z = 5)
+    
+    voxelgrid = point_cloud.structures[voxelgrid_id]
+    
+    sample = point_cloud.get_sample("voxelgrid_centers", voxelgrid_id = voxelgrid_id)
+
+for j in range(15):
+    start = time.perf_counter()
+    
+    cpu_antes = process.cpu_times()
+    
+    voxelgrid_id = point_cloud.add_structure("voxelgrid", size_x = 5, size_y = 5, size_z = 5)
+    
+    voxelgrid = point_cloud.structures[voxelgrid_id]
+    
+    sample = point_cloud.get_sample("voxelgrid_centers", voxelgrid_id = voxelgrid_id)
+    
+    cpu_final = process.cpu_times()
+    
+    end = time.perf_counter()
+    
+    tempo_total = (end-start)
+    cpu_total = ((cpu_final.user - cpu_antes.user) + (cpu_final.system - cpu_antes.system))
+    cpu_media = (cpu_total/(tempo_total)) * 100
+    
+    tempo.append(tempo_total)
+    cpus_porcentagem.append(cpu_media)
+    cpus_tempo.append(cpu_total)
+
+print(f"Tempo médio: {statistics.mean(tempo):.5f}s")
+print(f"Tempo mediano: {statistics.median(tempo):.5f}s")
+
+print(f"CPU média (%): {statistics.mean(cpus_porcentagem):.2f}%")
+print(f"CPU mediana (%): {statistics.median(cpus_porcentagem):.2f}%")
+
+print(f"CPU tempo médio: {statistics.mean(cpus_tempo):.5f}s")
